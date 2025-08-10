1143cbf449292d0a3a4138b574c76a65df8a2b3a
Matthew <xscytheric@gmail.com>
2025-08-10 15:37:52 -0400

    * Implement priority for providers in Nexus (higher priority loads first, respecting dependencies as well) * Bump Nexus minor version

--- Testing/Stories/DefaultStory.lua
+++ Testing/Stories/DefaultStory.lua
@@ -1,20 +1,19 @@
 return function(script)
-    local ReplicatedStorage = game:GetService("ReplicatedStorage")
+	local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-    return function(Target)
-        local t = task.spawn(function()
-            local TestEZ = require(ReplicatedStorage.TestEZ)
-            local ModuleNameToTest = string.gsub(script.Name, ".story", ""):lower()
-            local ModuleToTest = ReplicatedStorage.src:FindFirstChild(ModuleNameToTest):FindFirstChild("src")
-    
-            TestEZ.TestBootstrap:run({
-                ModuleToTest
-            })
-        end)
-        
-        return function()
-            task.cancel(t)
-        end
-    end
-    
+	return function(Target)
+		local t = task.spawn(function()
+			local TestEZ = require(ReplicatedStorage.TestEZ)
+			local ModuleNameToTest = string.gsub(script.Name, ".story", "")
+			local ModuleToTest = ReplicatedStorage.src:FindFirstChild(ModuleNameToTest):FindFirstChild("src")
+
+			TestEZ.TestBootstrap:run({
+				ModuleToTest,
+			})
+		end)
+
+		return function()
+			task.cancel(t)
+		end
+	end
 end
